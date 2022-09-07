236f6432ce4b99290cdd44e270f259d9cf59e70c
hgetov <h.getov@datamax.bg>
2022-09-07 14:59:07 +0300

    Add release log

--- app/src/main/java/com/example/resolutionapp/MainActivity.java
+++ app/src/main/java/com/example/resolutionapp/MainActivity.java
@@ -19,6 +19,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppUpdater appUpdater = new AppUpdater(this);
+        appUpdater.setUpdateFrom(UpdateFrom.GITHUB)
+                .setGitHubUserAndRepo("hristogetov","ResolutionApp")
+                .setDisplay(Display.SNACKBAR);//.setDuration(Duration.NORMAL);
+        appUpdater.setTitleOnUpdateAvailable("Update available")
+                .setContentOnUpdateAvailable("Check out the latest version available of my app!")
+                .setTitleOnUpdateNotAvailable("Update not available")
+                .setContentOnUpdateNotAvailable("No update available. Check for updates again later!")
+                .setButtonUpdate("Update now?")
+                .setIcon(R.drawable.ic_launcher_foreground) // Notification icon
+                .setCancelable(false);
+        appUpdater.start();
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int height = displayMetrics.heightPixels;
@@ -27,15 +39,10 @@ public class MainActivity extends AppCompatActivity {
         textView.setText("Resolution: \n" + "Height: " + height + "\n Width: " + width);
         TextView releaseView = findViewById(R.id.new_release);
         releaseView.setText("Version 1.2");
-        AppUpdater appUpdater = new AppUpdater(this);
-        appUpdater.setUpdateFrom(UpdateFrom.GITHUB)
-                .setGitHubUserAndRepo("HristoGetov","ResolutionApp")
-                .setDisplay(Display.SNACKBAR).setDuration(Duration.NORMAL)
-                .start();
        // Log.e("Resolution", "Height: " + height + ", Width: " + width);
-        Toast.makeText(this,"Resolution: " + "Height: " + height + ", Width: " + width, Toast.LENGTH_LONG).show();
-        Toast.makeText(this,"This is a message for new release! ", Toast.LENGTH_LONG).show();
-        Toast.makeText(this,"This is a message for new release version 1.2! ", Toast.LENGTH_LONG).show();
+//        Toast.makeText(this,"Resolution: " + "Height: " + height + ", Width: " + width, Toast.LENGTH_LONG).show();
+//        Toast.makeText(this,"This is a message for new release! ", Toast.LENGTH_LONG).show();
+//        Toast.makeText(this,"This is a message for new release version 1.2! ", Toast.LENGTH_LONG).show();
 
 
     }
