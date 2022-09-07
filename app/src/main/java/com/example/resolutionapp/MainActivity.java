1519d2d3483fc3c6678eef9a8b198bae76875b91
hgetov <h.getov@datamax.bg>
2022-09-07 13:29:41 +0300

    new version 1.2

--- app/src/main/java/com/example/resolutionapp/MainActivity.java
+++ app/src/main/java/com/example/resolutionapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 import com.github.javiersantos.appupdater.AppUpdater;
 import com.github.javiersantos.appupdater.enums.Display;
+import com.github.javiersantos.appupdater.enums.Duration;
 import com.github.javiersantos.appupdater.enums.UpdateFrom;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,15 +25,17 @@ public class MainActivity extends AppCompatActivity {
         int width = displayMetrics.widthPixels;
         TextView textView = findViewById(R.id.size);
         textView.setText("Resolution: \n" + "Height: " + height + "\n Width: " + width);
+        TextView releaseView = findViewById(R.id.new_release);
+        releaseView.setText("Version 1.2");
         AppUpdater appUpdater = new AppUpdater(this);
-        appUpdater.setUpdateFrom(UpdateFrom.GITHUB);
-        appUpdater.setGitHubUserAndRepo("hristogetov","ResolutionApp")
-                .setDisplay(Display.DIALOG).setCancelable(false)
+        appUpdater.setUpdateFrom(UpdateFrom.GITHUB)
+                .setGitHubUserAndRepo("HristoGetov","ResolutionApp")
+                .setDisplay(Display.SNACKBAR).setDuration(Duration.NORMAL)
                 .start();
        // Log.e("Resolution", "Height: " + height + ", Width: " + width);
         Toast.makeText(this,"Resolution: " + "Height: " + height + ", Width: " + width, Toast.LENGTH_LONG).show();
         Toast.makeText(this,"This is a message for new release! ", Toast.LENGTH_LONG).show();
-        Toast.makeText(this,"This is a message for new release version 1.1! ", Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"This is a message for new release version 1.2! ", Toast.LENGTH_LONG).show();
 
 
     }
